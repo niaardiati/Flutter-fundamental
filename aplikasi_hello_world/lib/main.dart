df7074a8c8dced5ce189d844696af98ccd6368bd
niaardiati <ardiatinia01@gmail.com>
2021-04-07 23:03:19 +0700

    Text widget

--- aplikasi_hello_world/lib/main.dart
+++ aplikasi_hello_world/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(new MyApp());
 }
 
@@ -10,11 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Aplikasi Hello World"),),
-        body: Center(child: Text("Hello world")),
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 100,
+                child: Text(
+                  "Saya sedang melatih kemampuan flutter saya.",
+                style: TextStyle(
+                  color: Colors.white, 
+                  fontStyle: FontStyle.italic, 
+                  fontWeight: FontWeight.w700, 
+                  fontSize: 20)
+                ))),
       ),
     );
   }
 }
-
-
