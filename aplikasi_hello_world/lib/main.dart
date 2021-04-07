b020c5b577904c991ab50ac48d5677aa67c40d46
niaardiati <ardiatinia01@gmail.com>
2021-04-07 21:26:58 +0700

    aplikasi pertamaku

---
+++ aplikasi_hello_world/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Aplikasi Hello World"),),
+        body: Center(child: Text("Hello world")),
+      ),
+    );
+  }
+}
+
+
